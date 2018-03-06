aecc9774c6a5621d1c5cb024c6f1633ae4ac008f
danilo.bernardi <danilo.bernardini93@gmail.com>
2018-03-06 18:40:29 +0100

    Add signals transfer functions

--- src/main/java/com/bernardini/vrphysioplatform/RealtimeFXMLController.java
+++ src/main/java/com/bernardini/vrphysioplatform/RealtimeFXMLController.java
@@ -1,10 +1,8 @@
 
 package com.bernardini.vrphysioplatform;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.Vector;
 import java.util.logging.Level;
@@ -18,18 +16,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
-import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import org.bytedeco.javacv.*;
@@ -57,6 +50,20 @@ public class RealtimeFXMLController implements Initializable {
             seriesEda, seriesEcg, seriesEmg, seriesForce, seriesResp, seriesEeg;
 
     private static final String STREAMING_SERVER_URL = "rtmp://127.0.0.1:1936/live";
+    private static final int N_BITS = 16;
+    private static final double A0 = 1.12764514e-3;
+    private static final double A1 = 2.34282709e-4;
+    private static final double A2 = 8.77303013e-8;
+    private static final int VCC = 3;
+    private static final double KELVIN_CELSIUS = 273.15;
+    private static final double C_MIN = 27400;
+    private static final double C_MAX = 39100;
+    private static final double EMG_ECG_GAIN = 1000;
+    private static final double EEG_GAIN = 40000;
+    private static final double EDA_CONST = 0.12;
+    private static final double POW_2_N = Math.pow(2, N_BITS);
+    
+    
     Logger log = Logger.getLogger(OfflineFXMLController.class.getName());
     private boolean play = false;
     private boolean stop = false;
@@ -72,10 +79,10 @@ public class RealtimeFXMLController implements Initializable {
         
         /*      SETUP SENSORS     */
         
-        seriesX = new XYChart.Series<>();
-        seriesX.setName("X");
-        seriesY = new XYChart.Series<>();
-        seriesY.setName("Y");
+//        seriesX = new XYChart.Series<>();
+//        seriesX.setName("X");
+//        seriesY = new XYChart.Series<>();
+//        seriesY.setName("Y");
         seriesZ = new XYChart.Series<>();
         seriesZ.setName("Z");
         seriesTemp = new XYChart.Series<>();
@@ -94,28 +101,28 @@ public class RealtimeFXMLController implements Initializable {
         seriesResp.setName("Resp");
         
         for (int i = 0; i < 10; i++){
-            seriesX.getData().add(new XYChart.Data<>(i + "", 0));
-            seriesY.getData().add(new XYChart.Data<>(i + "", 0));
+//            seriesX.getData().add(new XYChart.Data<>(i + "", 0));
+//            seriesY.getData().add(new XYChart.Data<>(i + "", 0));
             seriesZ.getData().add(new XYChart.Data<>(i + "", 0));
             seriesTemp.getData().add(new XYChart.Data<>(i + "", 0));
-//            seriesEcg.getData().add(new XYChart.Data<>(i + "", 0));
-//            seriesEda.getData().add(new XYChart.Data<>(i + "", 0));
-//            seriesEeg.getData().add(new XYChart.Data<>(i + "", 0));
-//            seriesEmg.getData().add(new XYChart.Data<>(i + "", 0));
-//            seriesForce.getData().add(new XYChart.Data<>(i + "", 0));
-//            seriesResp.getData().add(new XYChart.Data<>(i + "", 0));
+            seriesEcg.getData().add(new XYChart.Data<>(i + "", 0));
+            seriesEda.getData().add(new XYChart.Data<>(i + "", 0));
+            seriesEeg.getData().add(new XYChart.Data<>(i + "", 0));
+            seriesEmg.getData().add(new XYChart.Data<>(i + "", 0));
+            seriesForce.getData().add(new XYChart.Data<>(i + "", 0));
+            seriesResp.getData().add(new XYChart.Data<>(i + "", 0));
         }
         
-        accChart.getData().add(seriesX);
-        accChart.getData().add(seriesY);
+//        accChart.getData().add(seriesX);
+//        accChart.getData().add(seriesY);
         accChart.getData().add(seriesZ);
         tempChart.getData().add(seriesTemp);
-//        ecgChart.getData().add(seriesEcg);
-//        emgChart.getData().add(seriesEmg);
-//        eegChart.getData().add(seriesEeg);
-//        edaChart.getData().add(seriesEda);
-//        forceChart.getData().add(seriesForce);
-//        respChart.getData().add(seriesResp);
+        ecgChart.getData().add(seriesEcg);
+        emgChart.getData().add(seriesEmg);
+        eegChart.getData().add(seriesEeg);
+        edaChart.getData().add(seriesEda);
+        forceChart.getData().add(seriesForce);
+        respChart.getData().add(seriesResp);
               
    
         try {
@@ -133,7 +140,7 @@ public class RealtimeFXMLController implements Initializable {
             for (int i = 0; i < 1000; i++)
                frames[i] = new Device.Frame();
 
-            dev.BeginAcq(1000, 0xFF, 16); // 1000 Hz, 8 channels, 16 bits
+            dev.BeginAcq(1000, 0xFF, N_BITS); // 1000 Hz, 8 channels, 16 bits
 
         }
         catch (BPException err){
@@ -146,27 +153,31 @@ public class RealtimeFXMLController implements Initializable {
                 try {
                     dev.GetFrames(1000, frames); 
                  
-                    if (seriesX.getData().size() >= 10){
-                        seriesX.getData().remove(0);
-                        seriesY.getData().remove(0);
+                    if (seriesZ.getData().size() >= 10){
+//                        seriesX.getData().remove(0);
+//                        seriesY.getData().remove(0);
                         seriesZ.getData().remove(0);
                         seriesTemp.getData().remove(0);
-//                        seriesEcg.getData().remove(0);
-//                        seriesEda.getData().remove(0);
-//                        seriesEeg.getData().remove(0);
-//                        seriesEmg.getData().remove(0);
-//                        seriesResp.getData().remove(0);
-//                        seriesForce.getData().remove(0);
+                        seriesEcg.getData().remove(0);
+                        seriesEda.getData().remove(0);
+                        seriesEeg.getData().remove(0);
+                        seriesEmg.getData().remove(0);
+                        seriesResp.getData().remove(0);
+                        seriesForce.getData().remove(0);
                     }
+                    
+                    Device.Frame data = frames[0];
 
-                    seriesX.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[0]));
-                    seriesY.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[1]));
-                    seriesZ.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[2]));
-                    seriesTemp.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[3]));
-//                    seriesEcg.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[4]));
-//                    seriesEeg.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[5]));
-//                    seriesResp.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[6]));
-//                    seriesEda.getData().add(new XYChart.Data<>(signalsCounter + "", frames[0].an_in[7]));
+//                    seriesX.getData().add(new XYChart.Data<>(signalsCounter + "", transAcc(data.an_in[0])));
+                    seriesEeg.getData().add(new XYChart.Data<>(signalsCounter + "", transEeg(data.an_in[1])));
+//                    seriesY.getData().add(new XYChart.Data<>(signalsCounter + "", transAcc(data.an_in[1])));
+                    seriesEcg.getData().add(new XYChart.Data<>(signalsCounter + "", transEcg(data.an_in[0])));
+                    seriesZ.getData().add(new XYChart.Data<>(signalsCounter + "", transAcc(data.an_in[2])));
+                    seriesTemp.getData().add(new XYChart.Data<>(signalsCounter + "", transTemp(data.an_in[3])));
+                    seriesResp.getData().add(new XYChart.Data<>(signalsCounter + "", transResp(data.an_in[4])));
+                    seriesForce.getData().add(new XYChart.Data<>(signalsCounter + "", data.an_in[5]));
+                    seriesEmg.getData().add(new XYChart.Data<>(signalsCounter + "", transEmg(data.an_in[6])));
+                    seriesEda.getData().add(new XYChart.Data<>(signalsCounter + "", transEda(data.an_in[7])));
                             
                     signalsCounter++;
                         
@@ -206,6 +217,53 @@ public class RealtimeFXMLController implements Initializable {
         t.setDaemon(true);
         t.start();
 
+    }
+    
+    private double transTemp(int rawData){
+        double ntcV = rawData*VCC/POW_2_N;
+        double ntcO = 10000*ntcV/(VCC-ntcV);
+
+        double logO = Math.log(ntcO);
+
+        double tempK = 1/(A0+(A1*logO)+(A2*Math.pow(logO,3)));
+        double tempC = tempK-KELVIN_CELSIUS;
+        
+        return tempC;
+    }
+    
+    private double transAcc(int rawData){
+        double acc = (2*(rawData-C_MIN)/(C_MAX-C_MIN))-1;
+        return acc;
+    }
+    
+    private double transResp(int rawData){
+        double perc = ((rawData/POW_2_N)-0.5)*100;
+        return perc;
+    }
+    
+    private double transEmg(int rawData){
+        double emgV = (((rawData/POW_2_N)-0.5)*VCC)/EMG_ECG_GAIN;
+        double emgMV = emgV*1000;
+        return emgMV;
+    }
+    
+    private double transEda(int rawData){
+        double edaMS = (rawData/POW_2_N*VCC)/EDA_CONST;
+        return edaMS;
+    }
+    
+    private double transEcg(int rawData){
+        double ecgV = (((rawData/POW_2_N)-0.5)*VCC)/EMG_ECG_GAIN;
+        double ecgMV = ecgV*1000;
+        return ecgMV;
+    }
+    
+    private double transEeg(int rawData){
+        double eegV = (((rawData/POW_2_N)-0.5)*VCC)/EEG_GAIN;
+        double eegMV = eegV*1000000;
+        System.out.println("rawData: " + rawData);
+        System.out.println("EEG(mV): " + eegMV);
+        return eegMV;
     }
     
     protected void stopAcquisitions(){
